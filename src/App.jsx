788a4fb2864157a4ae1aed3e51ec6afb9ed4f889
TATIANA MARQUES DOMINGOS <tatiana_m_domingos@estudante.sesisenai.org.br>
2024-09-19 22:06:33 -0300

    first commit :tada

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+   return (
+   <>
+   <header>Cabeçalho</header>
+   <aside>Lado Esquerdo</aside>
+   <main>Principal</main>
+   <footer>Rodapé</footer>
+   
+   </>
+   )
+}
+
+export default App
