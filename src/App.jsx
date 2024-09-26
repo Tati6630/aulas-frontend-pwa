d9696e07c7b7b93d66f7b94e0f616d274422b264
Tati6630 <tati.m.domingos@gmail.com>
2024-09-25 22:25:07 -0300

    separação de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import Cabecalho from './comum/componentes/Cabecalho/Cabecalho'
+import Principal from './comum/componentes/Principal/Principal'
+import Rodape from './comum/componentes/Rodape/Rodape'
+
+
 
 function App() {
    return (
    <>
-   <header>Cabeçalho</header>
-   <aside>Lado Esquerdo</aside>
-   <main>Principal</main>
-   <footer>Rodapé</footer>
+   <Cabecalho />
+   <Principal />
+   <Rodape />
    
    </>
    )
